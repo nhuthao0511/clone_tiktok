0eeb4691f5675c13cdb3960a2005c9819daaa33d
nhuthao0511 <haotran9871@gmail.com>
2025-05-25 20:41:48 +0700

    first commit

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,17 +1,41 @@
-import classNames from 'classnames/bind.js'
-import styles from './Header.module.scss'
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '../../../../asset/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Header() {
-    return (
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}>
-                {/* {logo} */}
-                {/* {search} */}
-            </div> 
-        </header>
-    )
+  const handleSearch = () => {
+    console.log('Search clicked');
+  };
+
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt='tiktok' />
+        </div>
+
+        <div className={cx('main_search')}>
+          <input placeholder='Search accounts or videos' spellCheck={false} />
+
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} className={cx('btn-clse')} />  
+          </button> 
+
+          <FontAwesomeIcon icon={faSpinner} className={cx('loading', 'fa-spin')} />   
+           
+
+          <button className={cx('search-btn')} onClick={handleSearch}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className={cx('chidr-search')}/>
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
